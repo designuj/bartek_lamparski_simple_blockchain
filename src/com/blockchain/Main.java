c16f4a630e1ccf24b06d8d57abbccd5afadc935f
designuj <designuj@fejm.pl>
2018-05-05 08:34:26 +0200

    First blocks

--- src/com/blockchain/Main.java
+++ src/com/blockchain/Main.java
@@ -3,6 +3,13 @@ package com.blockchain;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Block firstBlock = new Block("lorem ipsum", "0");
+        System.out.println("Hash for 1 block: " + firstBlock.hash);
+
+        Block secondBlock = new Block("lorem ipsum", firstBlock.hash);
+        System.out.println("Hash for 2 block: " + secondBlock.hash);
+
+        Block thirdBlock = new Block("lorem ipsum", secondBlock.hash);
+        System.out.println("Hash for 3 block: " + thirdBlock.hash);
     }
 }
